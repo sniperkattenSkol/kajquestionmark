44ef201df6bf6d42e584cc23bebc34eb3390c897
sniperkattenSkol <te15.nils.lindfors@umea.nti.se>
2017-05-18 11:22:50 +0200

    Ideer

--- kajJs.js
+++ kajJs.js
@@ -2,5 +2,12 @@ setInterval(function(){spinss();},1000);
 	var x = 0;
 	function spinss() {
 		x++;
+
+		if (x>=1000) {
+				document.getElementById("dspnumb").innerHTML = x + " Har du Inget bättre för dig?";
+		}else if (x>=100) {
+			document.getElementById("dspnumb").innerHTML = x + " sekunder borta...";
+		} else {
 		document.getElementById("dspnumb").innerHTML = x;
 	}
+}
